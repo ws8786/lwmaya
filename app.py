d311601ded1e12b5d9cf439ba5091d35b2f10aac
Kirill Kovalevskiy <kovalewskiy@gmail.com>
2016-03-18 11:20:08 -0700

    Added vray init

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
-print "Hello lw-maya!"
+import pymel.core as pm
+
+class VRayConfig(object):
+    """
+    This class responsible for linear workflow
+    VRaya configurtaion
+    """
+    def __init__(self):
+        pass
+
+    def set_linear_settings(self):
+
+        # Make sure that VRay plugin loaded
+        # and render tab set to vray
+        pm.mel.loadPlugin('vray')
+        current_renderer = pm.Attribute('defaultRenderGlobals.currentRenderer')
+        current_renderer.set('vray')
+
+    def linearize_texture(self, file_node):
+        pass
+
+    def linearize_all_textures(self):
+        pass
+
+def main():
+    vr_conf = VRayConfig()
+    vr_conf.set_linear_settings()
